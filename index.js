8e078a1545bd2617e9315903c836a8b881c9f791
Oelin <42823429+Oelin@users.noreply.github.com>
2021-05-03 12:49:44 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+async function record(seconds, fps=10, el=document.body) {
+
+  let frames = []
+  let script = document.createElement('script')
+  let sleep = ms => new Promise(r => setTimeout(r, ms))
+  
+  script.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
+  body.appendChild(script)  
+  await sleep(1000)
+  
+  let end = Date.now() + seconds * 1000
+  
+  while (Date.now() < end) {
+    html2canvas(el).then(canvas => frames.push(canvas))
+    await sleep(1000 / fps)
+  }
+  
+  return frames
+}
